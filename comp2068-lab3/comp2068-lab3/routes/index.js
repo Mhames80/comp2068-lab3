bc17b90e0deda23c2b80bd6659d5089fe52d46bf
Matt <mhames80@gmail.com>
2020-06-11 17:44:17 -0400

    finished lab 3

--- comp2068-lab3/comp2068-lab3/routes/index.js
+++ comp2068-lab3/comp2068-lab3/routes/index.js
@@ -4,7 +4,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Nuclear Family' });
+});
+router.get('/yolanda/', function (req, res) {
+    res.render('yolanda', { title: 'Yolanda' });
+});
+router.get('/matt/', function (req, res) {
+    res.render('matt', { title: 'Matt' });
+});
+router.get('/mark/', function (req, res) {
+    res.render('mark', { title: 'Mark' });
+});
+router.get('/chantel/', function (req, res) {
+    res.render('chantel', { title: 'Chantel' });
 });
 
 module.exports = router;
--- comp2068-lab3/comp2068-lab3/routes/users.js
+++ comp2068-lab3/comp2068-lab3/routes/users.js
@@ -4,7 +4,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res) {
-    res.send('respond with a resource');
+    var potentialNames = ['Jacob', 'Bob', 'Mike'];
+    var randomName = potentialNames[Math.floor(Math.random() * potentialNames.length)];
+    res.render('users', { 'name': randomName });
 });
 
 module.exports = router;
